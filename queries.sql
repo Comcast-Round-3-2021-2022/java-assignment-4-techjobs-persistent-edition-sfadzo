7b00832f60f7e6c293e38b589f1f8f94c763ef2f
Paul Rodriguez <paul_rodriguez@comcast.com>
2022-09-19 08:16:45 -0600

    part 1 and 2 done

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
 -- Part 1: Test it with SQL
-
+Id int
+employer varchar
+name varchar
+skills varchar
 -- Part 2: Test it with SQL
+select name from employer
+where location = "St. Louis City";
+
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.Size;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 150, message = "Name must be between 3 and 150 characters")
     private String name;
 
     public int getId() {
